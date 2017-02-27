83038c22868c648c289b3e277a6404c783e87c1b
唐鹏 <tang.peng@hotmail.com>
2017-02-27 17:57:50 +0800

    Update

--- src/com/tang/Main.java
+++ src/com/tang/Main.java
@@ -9,27 +9,3 @@ public class Main {
 
 
 }
-
-class ArrayBub {
-    private long[] a;
-    private int nElems;
-
-    public ArrayBub(int max){
-        a = new long[max];
-        nElems = 0;
-    }
-
-    public void insert(long value){
-        a[nElems] = value;
-        nElems++;
-    }
-
-    public void display(){
-        for(int j = 0; j<nElems;j++){
-            System.out.print(a[j]+ " ");
-        }
-        System.out.println("");
-    }
-
-}
-
